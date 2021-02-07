397f919c324918d5470f88aef14c14bf43e2b3bc
ipolo0000i <36645557+ipolo0000i@users.noreply.github.com>
2021-02-07 21:54:51 +0100

    anonymisation du fichier config

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 #configuration
 ##Obligatoire
-lien_meteociel = 'https://www.meteociel.fr/previsions/25600/grigny.htm'
-station = "Grigny"
+lien_meteociel = 'https://www.meteociel.fr/previsions/2xxxx'
+station = "Station météo"
 
 ##facultative uniquement pour notification mail
 e_mail = "email@domaine.dn"#adresse email pour la reception des notifications
 e_mail_envoi = 'email@gmail.com'#adresse e-mail d'envoi des notifications
 mot_de_passe = 'motdepasse'#mot de passe e-email d'envoi des notifications
-
