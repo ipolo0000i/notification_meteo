e261c537278677ffff58b86f49259e0e96d9f4ba
Polo0000 <pauldutronc@gmail.com>
2021-02-07 21:50:34 +0100

    correction erreur config

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ lien_meteociel = 'https://www.meteociel.fr/previsions/25600/grigny.htm'
 station = "Grigny"
 
 ##facultative uniquement pour notification mail
+e_mail = "email@domaine.dn"#adresse email pour la reception des notifications
 e_mail_envoi = 'email@gmail.com'#adresse e-mail d'envoi des notifications
 mot_de_passe = 'motdepasse'#mot de passe e-email d'envoi des notifications
 
